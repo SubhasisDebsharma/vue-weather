756d14713d523d94b371ac047c1e0b8e82615e35
Subhasis Debsharma(IN4424) <M1044424@mindtree.com>
2019-07-10 02:16:12 +0530

    so so

--- view-weather-functions/functions/index.js
+++ view-weather-functions/functions/index.js
@@ -1,46 +1,166 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-var serviceAccount = require("./viewweather-a2bc9-firebase-adminsdk-um2qb-acadb128d0.json");
+const express = require("express");
+const cors = require("cors");
+const expressApp = express();
+const serviceAccount = require("./viewweather-a2bc9-firebase-adminsdk-um2qb-acadb128d0.json");
 
-var app = admin.initializeApp({
+const firebaseApp = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://viewweather-a2bc9.firebaseio.com"
 });
-// const { Logging } = require("@google-cloud/logging");
-// const logging = new Logging();
-// const log = logging.log("view-weather-log");
 
-// const METADATA = {
-//   resource: {
-//     type: "cloud_function",
-//     labels: {
-//       function_name: "populateSearchDB",
-//       region: "us-central1"
-//     }
-//   }
-// };
-// exports.helloWorldl;
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+expressApp.use(cors({ origin: true }));
+
+const cityList = [
+  {
+    coord: { lat: 13.13333, lon: 78.133331 },
+    country: "IN",
+    id: 1266305,
+    name: "Kolar"
+  },
+  {
+    coord: { lat: 49.80743, lon: 21.434071 },
+    country: "PL",
+    id: 768827,
+    name: "Kolaczyce"
+  },
+  {
+    coord: { lat: 15.2, lon: 76.866669 },
+    country: "IN",
+    id: 1266320,
+    name: "Kolagallu"
+  }
+];
+
+const citySearchResult = {
+  "19099": {
+    coord: { lat: 68.880623, lon: 33.018421 },
+    country: "RU",
+    id: 546554,
+    name: "Kola"
+  },
+  "22532": {
+    coord: { lat: 55.328335, lon: 37.738888 },
+    country: "RU",
+    id: 6416531,
+    name: "Kolachëvo"
+  },
+  "39016": {
+    coord: { lat: -24.9, lon: 152.083328 },
+    country: "AU",
+    id: 2161177,
+    name: "Kolan"
+  },
+  "43763": {
+    coord: { lat: 13.13333, lon: 78.133331 },
+    country: "IN",
+    id: 1266305,
+    name: "Kolar"
+  },
+  "97486": {
+    coord: { lat: 49.80743, lon: 21.434071 },
+    country: "PL",
+    id: 768827,
+    name: "Kolaczyce"
+  },
+  "106036": {
+    coord: { lat: 15.2, lon: 76.866669 },
+    country: "IN",
+    id: 1266320,
+    name: "Kolagallu"
+  },
+  "112040": {
+    coord: { lat: 25.23333, lon: 77.599998 },
+    country: "IN",
+    id: 1266302,
+    name: "Kolaras"
+  },
+  "184843": {
+    coord: { lat: 5.07028, lon: 119.896942 },
+    country: "PH",
+    id: 1708553,
+    name: "Kolape"
+  },
+  "184844": {
+    coord: { lat: 8.1144, lon: 123.897102 },
+    country: "PH",
+    id: 1708557,
+    name: "Kolambugan"
+  },
+  "188209": {
+    coord: { lat: 52.217388, lon: 17.62413 },
+    country: "PL",
+    id: 3090207,
+    name: "Kolaczkowo"
+  }
+};
+
+const isAuthorized = function(req, res, next) {
+  admin
+    .auth()
+    .verifyIdToken(req.headers.authorization)
+    .then(decodedIdToken => {
+      if (decodedIdToken) {
+        req.user = decodedIdToken;
+        return next();
+      }
+      res.status(401);
+      return res.send("Invalid user");
+    })
+    .catch(error => {
+      res.status(401);
+      res.send("Invalid user");
+    });
+};
+
+expressApp.post("/addCityToMyList", isAuthorized, (req, res) => {
+  firebaseApp
+    .database()
+    .ref(`myCityList/${req.user.uid}/${req.body.id}`)
+    .set(req.body, error => {
+      if (error) {
+        res.status(500);
+        res.send("Failed to store in firebase");
+      } else {
+        res.send({
+          success: true
+        });
+      }
+    });
+});
+
+expressApp.get("/getMyCityList", isAuthorized, (req, res) => {
+  firebaseApp
+    .database()
+    .ref(`myCityList/${req.user.uid}`)
+    .once("value", data => {
+      res.send(data || []);
+    });
+});
+
+expressApp.delete("/removeCityFromList", isAuthorized, (req, res) => {
+  firebaseApp
+    .database()
+    .ref(`myCityList/${req.user.uid}/${req.body.id}`)
+    .remove(error => {
+      if (error) {
+        res.status(500);
+        res.send("Failed to remove city");
+      } else {
+        res.send({
+          success: true
+        });
+      }
+    });
+});
+
+expressApp.get("/test", (req, res) => {
+  res.send("Hello World: Express app is working");
+});
 
 exports.helloWorld = functions.https.onRequest((req, res) => {
   res.send("Hello World: Testing View Weather");
-});
-
-exports.setLowerString = functions.https.onRequest((req, res) => {
-  // admin
-  //   .database()
-  //   .ref("cities")
-  //   .limitToFirst(1)
-  //   .on("value", (data, b) => {
-  //     console.log("data", data, b);
-  //     res.send(data);
-  //   });
-  res.send("not implementd");
 });
 
 exports.searchCities = functions.https.onRequest((req, res) => {
@@ -56,75 +176,20 @@ exports.searchCities = functions.https.onRequest((req, res) => {
     .ref("cities")
     .orderByChild("name")
     .startAt(properInput)
-    .limitToFirst(10)
-    .on("value", data => {
+    .limitToFirst(50)
+    .once("value", data => {
       res.send(data);
     });
 });
 
-exports.dummySearchCities = functions.https.onRequest((req, res) => {
-  const result = {
-    "19099": {
-      coord: { lat: 68.880623, lon: 33.018421 },
-      country: "RU",
-      id: 546554,
-      name: "Kola"
-    },
-    "22532": {
-      coord: { lat: 55.328335, lon: 37.738888 },
-      country: "RU",
-      id: 6416531,
-      name: "Kolachëvo"
-    },
-    "39016": {
-      coord: { lat: -24.9, lon: 152.083328 },
-      country: "AU",
-      id: 2161177,
-      name: "Kolan"
-    },
-    "43763": {
-      coord: { lat: 13.13333, lon: 78.133331 },
-      country: "IN",
-      id: 1266305,
-      name: "Kolar"
-    },
-    "97486": {
-      coord: { lat: 49.80743, lon: 21.434071 },
-      country: "PL",
-      id: 768827,
-      name: "Kolaczyce"
-    },
-    "106036": {
-      coord: { lat: 15.2, lon: 76.866669 },
-      country: "IN",
-      id: 1266320,
-      name: "Kolagallu"
-    },
-    "112040": {
-      coord: { lat: 25.23333, lon: 77.599998 },
-      country: "IN",
-      id: 1266302,
-      name: "Kolaras"
-    },
-    "184843": {
-      coord: { lat: 5.07028, lon: 119.896942 },
-      country: "PH",
-      id: 1708553,
-      name: "Kolape"
-    },
-    "184844": {
-      coord: { lat: 8.1144, lon: 123.897102 },
-      country: "PH",
-      id: 1708557,
-      name: "Kolambugan"
-    },
-    "188209": {
-      coord: { lat: 52.217388, lon: 17.62413 },
-      country: "PL",
-      id: 3090207,
-      name: "Kolaczkowo"
-    }
-  };
+exports.getMyCityList = functions.https.onRequest((req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
-  res.send(result);
+  res.send(cityList);
 });
+
+exports.dummySearchCities = functions.https.onRequest((req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.send(citySearchResult);
+});
+
+exports.widgets = functions.https.onRequest(expressApp);
